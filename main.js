ecc95f7be1f245397f6bc2f418159627ca8d8c7f
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2020-11-19 22:44:52 +0200

    scales menu implemented

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+// GO TO *__* FUNCTIONS
+
 function loadmenu() {
     console.log("clicked");
     location.href="menu.html";
 }
+
+function backtostart() {
+    console.log("clicked");
+    location.href="index.html";
+}
+
+function scales() {
+    console.log("clicked");
+    location.href="scales.html";
+}
+
+// SCALE/CHORDS/MODES FUNCTIONS
+
+// reloads scale display everytime an attribute is changed
+function changeDisplayScale() {
+    var scale = "TODO: getStringScale();";
+    document.getElementById("scaledisplay").innerHTML = scale;
+  }
